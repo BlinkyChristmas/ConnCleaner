28626002c78ae8e62d908a254f51cb5e3c9afb88
charles <128108973+OldFoolAgain@users.noreply.github.com>
2024-01-31 06:20:37 -0500

    cmake/gitignore

--- ConnCleaner/main.cpp
+++ ConnCleaner/main.cpp
@@ -1,14 +1,39 @@
-//
-//  main.cpp
-//  ConnCleaner
-//
-//  Created by Charles Kerr on 1/30/24.
-//
 
+#include <cstdlib>
 #include <iostream>
+#include <string>
+#include <stdexcept>
+#include <filesystem>
 
+#include "ConnectionStatus.hpp"
+using namespace std::string_literals ;
+
+// ========================================================================
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+    auto exitcode = EXIT_SUCCESS ;
+    try {
+        if (argc != 3) {
+            throw std::runtime_error("hours to keep and connect log path required as input arguments") ;
+        }
+        auto hours  = std::stoi(argv[1],nullptr,0) ;
+        auto logfile = std::filesystem::path(argv[2]) ;
+        if (!std::filesystem::exists(logfile)) {
+            std::clog << "Connection log does not exist, nothing to be done: "s << logfile.string() << std::endl;
+            return exitcode ;
+        }
+        auto status = ConnectionStatus(logfile) ;
+        if (!status.save(logfile,hours)) {
+            throw std::runtime_error("Unable to saee to: "s + logfile.string()) ;
+        }
+    }
+    catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        exitcode = EXIT_FAILURE ;
+    }
+    catch (...) {
+        std::cerr << "Unknown error!" << std::endl;
+        exitcode = EXIT_FAILURE ;
+    }
+    return exitcode;
 }
